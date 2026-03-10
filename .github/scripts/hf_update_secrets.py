3533a196ed1648622dc73c6b62e20069a6eaf88f
SLA2009 <sebastien.lamort@outlook.com>
2026-03-10 15:14:18 +0100

    MLflow Secrets map

--- .github/scripts/hf_update_secrets.py
+++ .github/scripts/hf_update_secrets.py
@@ -38,7 +38,7 @@ def get_service_secrets(service):
     # Mapping of service -> environment variables
     service_secrets_map = {
         "Airflow": ["AIRFLOW_POSTGRES_CONN_ID"],
-        "MLflow": ["MLFLOW_POSTGRESQL_URI, MLFLOW_S3_ENDPOINT_URL"],
+        "MLflow": ["MLFLOW_POSTGRESQL_URI", "MLFLOW_S3_ENDPOINT_URL"],
         "JupyterLab": ["JUPYTER_TOKEN"],
         "Streamlit": ["STREAMLIT_SERVER_HEADLESS", "STREAMLIT_LOGGER_LEVEL"],
         "n8n": ["N8N_DB_POSTGRES_URI"],
